1129a3128de6399828c9a7b3c32d2bd7a39b53bc
kanchanaanil <kanchana.anil@gmail.com>
2025-11-06 21:42:21 +0530

    Update test_ui_filters.py

--- tests/test_ui_filters.py
+++ tests/test_ui_filters.py
@@ -14,29 +14,39 @@ def test_filter_movies(driver, test_input):
     driver.get("https://tmdb-discover.surge.sh/")
 
     try:
-        # Category
+        # Category 
+        #//a[contains(text(),'Top rated')]
         logger.debug(f"Selecting category: {test_input['category']}")
-        driver.find_element(By.XPATH, f"//button[contains(text(),'{test_input['category']}')]").click()
+        driver.find_element(By.XPATH, f"//a[contains(text(),'{test_input['category']}')]").click()
 
         # Type
+        #//input[@id='react-select-2-input']
         logger.debug(f"Selecting type: {test_input['type']}")
-        driver.find_element(By.XPATH, f"//button[contains(text(),'{test_input['type']}')]").click()
+        year_input_from = driver.find_element(By.XPATH,//input[@id='react-select-2-input'])      
+        year_input_from.clear()
+        year_input.send_keys(test_input["type"])
 
         # Year
         logger.debug(f"Entering year: {test_input['year']}")
-        year_input = driver.find_element(By.ID, "year")
-        year_input.clear()
+       # year_input = driver.find_element(By.ID, "year")
+        year_input_from = driver.find_element(By.XPATH,//input[@id='react-select-4-input'])      
+        year_input_from.clear()
+        year_input.send_keys(test_input["year"])
+        
+        year_input_to = driver.find_element(By.XPATH,//input[@id='react-select-5-input'])
+        year_input_to.clear()
         year_input.send_keys(test_input["year"])
 
         # Rating
         logger.debug(f"Entering rating: {test_input['rating']}")
-        rating_input = driver.find_element(By.ID, "rating")
-        rating_input.clear()
-        rating_input.send_keys(test_input["rating"])
+        rating_input = driver.find_element(By.XPATH, "//div[@role='radio' and @aria-checked='true' and @aria-posinset='5']")
+        rating_input.click()
 
         # Genre
         logger.debug(f"Selecting genre: {test_input['genre']}")
-        driver.find_element(By.XPATH, f"//button[contains(text(),'{test_input['genre']}')]").click()
+        genre = driver.find_element(By.XPATH, //input[@id="react-select-3-input"])      
+        genre.clear()
+        genre.send_keys(test_input["genre"])
 
         results = driver.find_elements(By.CLASS_NAME, "card")
         logger.info(f"Found {len(results)} results for {test_input}")
